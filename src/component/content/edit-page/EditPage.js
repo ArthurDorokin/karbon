0ef1090413245cf0019ad1e131667743637c1c63
ArthurDorokhin <a.u.dorokhin@gmail.com>
2021-02-02 11:30:37 +0200

    edit page

--- src/component/content/edit-page/EditPage.js
+++ src/component/content/edit-page/EditPage.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {useSelector} from "react-redux";
 
 const EditPage = () => {
+    const editPageData = useSelector(state => state.editPageData.editPageData);
+    console.log(editPageData);
     return (
-        <div>edit page</div>
+        <div className="edit-page">
+            <div className="driver">driver - {editPageData.driver}</div>
+            <div className="modName">modName - {editPageData.modName}</div>
+            <div className="mod-version">modVersion - {editPageData.modVersion}</div>
+            <div className="port">port - {editPageData.port}</div>
+            <div className="status">status - {editPageData.status}</div>
+            <div className="status-descr">statusDescr - {editPageData.statusDescr}</div>
+        </div>
     )
 }
 
--- src/component/redux/reducers/rootReducer.js
+++ src/component/redux/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux'
 import devicesData from "./devicesData";
+import editPageData from "./editPage";
 
 export default combineReducers ({
-    data: devicesData
+    data: devicesData,
+    editPageData
 })
